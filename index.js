b731299b14cddb39c5ac1227cf734a52d305c8b8
Ichbinositu <hesbon.ezekiel@student.moringaschool.com>
2024-01-17 09:05:35 +0300

    Done with arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+
+                 
+ let random = Math.floor(Math.random() * 7)+7
+    
+const num3 = 9;
+const num4 = 5;
+let mod = num3-num4;
+
+
+let max = Math.max(0,3,5,7,12,17,20);
